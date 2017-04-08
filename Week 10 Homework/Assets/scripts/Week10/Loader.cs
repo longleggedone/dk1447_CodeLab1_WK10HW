0e8ac9b04a78c09bb25456ecc37a27feda65cb14
denniscarr <dcarr1@gmail.com>
2017-04-08 14:27:01 -0400

    It's done!

--- Week 10 Homework/Assets/scripts/Week10/Loader.cs
+++ Week 10 Homework/Assets/scripts/Week10/Loader.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Loader : MonoBehaviour {
 
-	void Start () {
-
+    void Start()
+    {
         AsteroidManager am = new AsteroidManager();
 
         AsteroidData[] asteroidDatas = am.ReadAsteroids();
 
-	}
+        // Instantiate all asteroids from asteroidDatas.
+        foreach(AsteroidData ad in asteroidDatas)
+        {
+            GameObject newAsteroid = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            newAsteroid.transform.position = ad.position;
+            newAsteroid.transform.localScale = ad.scale;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
